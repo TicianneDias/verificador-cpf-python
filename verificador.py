581f0ed97038ad7fd41a740bb8c744bc9d8e7fd2
Elienay Albuquerque <paulo.albuquerque@mv.com.br>
2022-03-23 10:58:10 -0300

    refactor: reorder code

--- verificador.py
+++ verificador.py
@@ -2,60 +2,58 @@
 
 
 def multiplica_digitos_parte_um(numeros):
-    """Multiplicação dos 9 primeiros valores de 10 a 2 e compara com o décimo
-    dígito do CPF."""
+    """Multiplicação dos 9 primeiros valores de 10 a 2 e compara com o décimo dígito do CPF."""
     cont = 10
     valor = 0
+
     for i in range(0, 9):
         mult = numeros[i] * cont
         cont -= 1
         valor += mult
+
     resto = (valor * 10) % 11
+
     if resto == 10:
-        resto = 0
-    if resto == numeros[9]:
-        return True
-    return False
+        return 0 == numeros[9]
+
+    return resto == numeros[9]
 
 
 def multiplica_digitos_parte_dois(numeros):
-    """Multiplicação dos 10 primeiros valores de 11 a 2 e compara com o último
-    dígito do CPF."""
+    """Multiplicação dos 10 primeiros valores de 11 a 2 e compara com o último dígito do CPF."""
     cont = 11
     valor = 0
+
     for i in range(0, 10):
         mult = numeros[i] * cont
         cont -= 1
         valor += mult
-    resto2 = (valor * 10) % 11
-    if resto2 == 10:
-        resto2 = 0
-    if resto2 == numeros[10]:
-        return True
-    return False
+
+    resto = (valor * 10) % 11
+
+    if resto == 10:
+        return 0 == numeros[10]
+
+    return resto == numeros[10]
 
 
-def cpf_invalido(numeros):
-    """Verifica se todos os dígitos são iguais e retorna CPF inválido"""
-    if numeros == numeros[::-1]:
-        return False
-    return True
+def cpf_valido(numeros):
+    """Verifica se todos os dígitos são iguais e retorna CPF válido"""
+    return numeros != numeros[::-1]
 
 
 def main():
-    numeros = []
-    cpf = input('Digite o cpf: ').replace('.', '').replace('-', '')
-    if len(cpf) == 11:
-        for numero in cpf:
-            numeros.append(int(numero))
-    (multiplica_digitos_parte_um(numeros))
-    (multiplica_digitos_parte_dois(numeros))
-    (cpf_invalido(numeros))
-    if multiplica_digitos_parte_dois(numeros) and multiplica_digitos_parte_dois(numeros) and cpf_invalido(
-            numeros) is False:
-        print('\033[31mCPF Inválido!')
-    else:
+    numeros = list(
+        map(int, list(input('Digite o cpf: ')
+                      .replace('.', '')
+                      .replace('-', ''))))
+
+    if multiplica_digitos_parte_um(numeros) \
+            and multiplica_digitos_parte_dois(numeros) \
+            and cpf_valido(numeros):
         print('\033[34mCPF Válido!')
+    else:
+        print('\033[31mCPF Inválido!')
 
 
 if __name__ == '__main__':
