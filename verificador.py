279dc9b8f10637be0f86eb45850be805a38a589b
Elienay Albuquerque <paulo.albuquerque@mv.com.br>
2022-03-23 10:37:05 -0300

    refactor: python guidelines

--- verificador.py
+++ verificador.py
@@ -1,18 +1,13 @@
 """Descubra se um cpf computado é válido ou não."""
-numeros = []
-cpf = input('Digite o cpf: ').replace('.', '').replace('-', '')
-if len(cpf) == 11:
-    for numero in cpf:
-        numeros.append(int(numero))
 
 
-def multiplica_digitos_parte_um(digitos):
+def multiplica_digitos_parte_um(numeros):
     """Multiplicação dos 9 primeiros valores de 10 a 2 e compara com o décimo
     dígito do CPF."""
     cont = 10
     valor = 0
     for i in range(0, 9):
-        mult = numeros[i]*cont
+        mult = numeros[i] * cont
         cont -= 1
         valor += mult
     resto = (valor * 10) % 11
@@ -23,13 +18,13 @@ def multiplica_digitos_parte_um(digitos):
     return False
 
 
-def multiplica_digitos_parte_dois(digitos):
+def multiplica_digitos_parte_dois(numeros):
     """Multiplicação dos 10 primeiros valores de 11 a 2 e compara com o último
     dígito do CPF."""
     cont = 11
     valor = 0
     for i in range(0, 10):
-        mult = numeros[i]*cont
+        mult = numeros[i] * cont
         cont -= 1
         valor += mult
     resto2 = (valor * 10) % 11
@@ -40,17 +35,28 @@ def multiplica_digitos_parte_dois(digitos):
     return False
 
 
-def cpf_invalido(digitos):
+def cpf_invalido(numeros):
     """Verifica se todos os dígitos são iguais e retorna CPF inválido"""
     if numeros == numeros[::-1]:
         return False
     return True
 
 
-(multiplica_digitos_parte_um(numeros))
-(multiplica_digitos_parte_dois(numeros))
-(cpf_invalido(numeros))
-if multiplica_digitos_parte_dois(numeros) and multiplica_digitos_parte_dois(numeros) and cpf_invalido(numeros) is False:
-    print('\033[31mCPF Inválido!')
-else:
-    print('\033[34mCPF Válido!')
+def main():
+    numeros = []
+    cpf = input('Digite o cpf: ').replace('.', '').replace('-', '')
+    if len(cpf) == 11:
+        for numero in cpf:
+            numeros.append(int(numero))
+    (multiplica_digitos_parte_um(numeros))
+    (multiplica_digitos_parte_dois(numeros))
+    (cpf_invalido(numeros))
+    if multiplica_digitos_parte_dois(numeros) and multiplica_digitos_parte_dois(numeros) and cpf_invalido(
+            numeros) is False:
+        print('\033[31mCPF Inválido!')
+    else:
+        print('\033[34mCPF Válido!')
+
+
+if __name__ == '__main__':
+    main()
